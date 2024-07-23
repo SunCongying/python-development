1aa7a4a2208ae0bddd6478fe781f5b6710193053
scy <2933...@qq.com>
2024-07-23 20:35:27 +0800

    定义视图函数index

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# view视图就是python函数，接收请求，处理，做出响应,用户输入的路由就是request
+# 路由是我们规定的
+from  django.http import HttpRequest
+from  django.http import HttpResponse
+def index(request):
+
+    return HttpResponse('ok')
