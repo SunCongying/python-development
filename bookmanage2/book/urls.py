c2fae2abb32777824b43e53bdcf4ef756bbe30a4
scy <2933...@qq.com>
2024-07-28 00:02:16 +0800

    获取URL路径中的参数

--- bookmanage2/book/urls.py
+++ bookmanage2/book/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from book.views import create_book
+from book.views import create_book,shop
 
 
 urlpatterns = [
     path('create/',create_book),
+    path('11000/11005/',shop),  # 如果有很多类似的路由，比如11004，11003等，定义一个符号'<city_id>/shop_id'
+    path('<city_id>/<shop_id>/',shop),  # view就要接收这两个参数
 ]
--- bookmanage2/book/views.py
+++ bookmanage2/book/views.py
@@ -10,5 +10,21 @@ def create_book(request):
         pub_date = '2024-1-1',
         readcount = 10,
     )
-
     return HttpResponse('create')
+
+
+def shop(request,city_id,shop_id):
+
+    print(city_id,shop_id)
+
+    return HttpResponse('贝贝的小饭店')
+
+"""
+查询字符串
+
+url以？为分割
+？   前面的是路径  
+    后面的是查询字符串，为字典形式，多个数据采用&拼接
+
+
+"""
